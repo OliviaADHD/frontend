9b75b8e255b8c0c9ada620eb55f79349f7f7c7ad
icodebyamanda <rasoamanda@gmail.com>
2021-10-20 23:43:22 +0200

    Period prediction intro page done - for review

--- navigators/RootStack.js
+++ navigators/RootStack.js
@@ -13,6 +13,7 @@ import Privacy from './../screens/Privacy';
 import Questionnaire from './../screens/Questionnaire';
 import Welcome_Post_Signup from './../screens/Welcome_Post_Signup';
 import CalendarInitial from './../screens/CalendarInitial';
+import Intro_Period_Prediction from './../screens/Intro_Period_Prediction';
 
 
 const Stack = createStackNavigator();
@@ -48,7 +49,9 @@ const RootStack = () => {
                 <Stack.Screen name='Welcome_Post_Signup' component={Welcome_Post_Signup} />
                 <Stack.Screen name="Questionnaire" component={Questionnaire} />
                 <Stack.Screen name="CalendarInitial" component={CalendarInitial} />
+                <Stack.Screen name="Intro_Period_Prediction" component={Intro_Period_Prediction} />
             </Stack.Navigator>
+            
         </NavigationContainer>
     )
 }
