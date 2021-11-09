1db56b0498e13bd4c1503c10e13e91c14afcc45a
icodebyamanda <rasoamanda@gmail.com>
2021-11-09 23:21:40 +0100

    state of the art - missing view changes when email has been submitted

--- screens/ResetPassword.js
+++ screens/ResetPassword.js
@@ -22,69 +22,70 @@ export default function ResetPassword({navigation}) {
 
   return (
   <StyledContainer>   
-  <StatusBar style="dark"/>
-  <InnerContainer>
+    <StatusBar style="dark"/>
+    <InnerContainer>
 
-    <MediumExtraText style={{ color : '#694398', marginTop : '20%', paddingBottom: '10%' }}>Forgot your password?</MediumExtraText>
-      
-    <BlockText style={{textAlign: 'center'}}>
-      Enter Your Registered Email Below To Receive Password Reset Instruction{"\n"}{"\n"}
-    </BlockText>
-      
-    <Formik
+      <MediumExtraText style={{ color : '#694398', marginTop : '20%', paddingBottom: '10%' }}>Forgot your password?</MediumExtraText>
+        
+      <BlockText style={{textAlign: 'center'}}>
+        Enter Your Registered Email Below To Receive Password Reset Instruction{"\n"}{"\n"}
+      </BlockText>
+        
+      <Formik
 
-      initialValues={{ email: '' }}
-      validate={values => {
-        const errors = {};
-        if (!values.email) {
-          errors.email = 'Required';
-        } 
-        return errors;
-      }}
-      onSubmit={(values, { setSubmitting }) => {
-        setTimeout(() => {
-          alert(JSON.stringify(values, null, 2));
-          setSubmitting(false);
-        }, 400);
-      }}
-    >{({    
-        values,
-        errors,
-        touched,
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        isSubmitting})=> (
-          
-          <StyledFormArea>
-            <View>
-          
-              <StyledTextInput
-                type="email"
-                name="email" 
-                placeholder="Email ID"
-                onChangeText={handleChange('email')}
-                onBlur={handleBlur('email')}
-                value={values.email}
-                keyboardType="email-address"
-              />
+        initialValues={{ email: '' }}
+        validate={values => {
+          const errors = {};
+          if (!values.email) {
+            errors.email = 'Required';
+          } 
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >{({    
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting})=> (
             
-              {errors.email && touched.email && 
-              <ErrorMessage>
-                <ErrorText>{errors.email}</ErrorText>
-              </ErrorMessage> }
+            <StyledFormArea>
+              <View>
+            
+                <StyledTextInput
+                  type="email"
+                  name="email" 
+                  placeholder="Email ID"
+                  onChangeText={handleChange('email')}
+                  onBlur={handleBlur('email')}
+                  value={values.email}
+                  keyboardType="email-address"
+                />
+              
+                {errors.email && touched.email && 
+                <ErrorMessage>
+                  <ErrorText>{errors.email}</ErrorText>
+                </ErrorMessage> }
 
-              <StyledButton type="submit" disabled={isSubmitting} onPress={handleSubmit}>
-                <ButtonText>Submit</ButtonText>
-              </StyledButton>
+                <StyledButton type="submit" disabled={isSubmitting} onPress={handleSubmit} style={{paddingTop:'5%', paddingBottom: '5%', marginTop: '10%'}}>
+                  <ButtonText>Submit</ButtonText>
+                </StyledButton>
 
-            </View>
-          </StyledFormArea>
-        )}
+              </View>
+            </StyledFormArea>
+          )}
 
-    </Formik>
- 
-  </InnerContainer>
+      </Formik>
+  
+    </InnerContainer>
   </StyledContainer>
-  )}
+  )
+}
 
