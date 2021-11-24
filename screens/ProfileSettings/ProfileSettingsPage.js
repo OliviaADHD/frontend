09b660e493c7aabd026ba422bfd52226162b0950
icodebyamanda <rasoamanda@gmail.com>
2021-11-24 22:52:31 +0100

    about to align image with data

--- screens/ProfileSettings/ProfileSettingsPage.js
+++ screens/ProfileSettings/ProfileSettingsPage.js
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import { View, Text, Image } from "react-native";
 import { StatusBar } from "expo-status-bar";
 
 import {
@@ -10,19 +11,28 @@ import {
   ButtonText,
 }from '../../components/styles';
 
-export default function ProfileSettingsPage({navigation, fullName, email}) {
-  const [profileData, setProfileData] = useState(fullName="Am", email="am@gmail.com")
+export default function ProfileSettingsPage({navigation, route}) {
+  // const {fullName, email, id} = route.params;
+  const [profileData, setProfileData] = useState(fullName="Samira Bahram", email="samirabahrampour@gmail.com")
   return (
     <StyledContainer> 
 
       <StatusBar style="dark"/>
 
       <InnerContainer>
-      <MediumExtraText>
-        {fullName}
-        {email}
 
-         Hello</MediumExtraText>
+        <View>
+
+        <Image source={require('../../assets/images/foxicon.png')} style={{width: 70, height: 70,}}/>
+
+        <View style={{flex: 1, paddingLeft: '25%', paddingTop: '15%'}}>
+          <Text style={{fontSize: 15}}>
+            {fullName}{"\n"}
+            {email}{"\n"}
+          </Text>
+        </View>
+
+    </View>
 
       </InnerContainer>
 
