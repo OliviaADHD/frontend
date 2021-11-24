013eb41ba679d25324e20ff33dcbcf4355018bcf
icodebyamanda <rasoamanda@gmail.com>
2021-11-24 22:10:31 +0100

    One way to have fullname and email

--- screens/ProfileSettings/ProfileSettingsPage.js
+++ screens/ProfileSettings/ProfileSettingsPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from "react";
 import { StatusBar } from "expo-status-bar";
 
 import {
@@ -10,15 +10,19 @@ import {
   ButtonText,
 }from '../../components/styles';
 
-export default function ProfileSettingsPage({navigation}) {
+export default function ProfileSettingsPage({navigation, fullName, email}) {
+  const [profileData, setProfileData] = useState(fullName="Am", email="am@gmail.com")
   return (
     <StyledContainer> 
 
       <StatusBar style="dark"/>
 
       <InnerContainer>
+      <MediumExtraText>
+        {fullName}
+        {email}
 
-        <MediumExtraText> Hello</MediumExtraText>
+         Hello</MediumExtraText>
 
       </InnerContainer>
 
