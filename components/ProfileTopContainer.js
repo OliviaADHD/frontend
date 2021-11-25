087574a961c78544791dd15e6c494ed1a21c7593
icodebyamanda <rasoamanda@gmail.com>
2021-11-25 15:57:11 +0100

    screen's code duplicated to component

---
+++ components/ProfileTopContainer.js
@@ -0,0 +1,83 @@
+import React, {useState} from "react";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+import {
+  StyledContainer,
+  InnerContainer,
+  Colors,
+}from '../../components/styles';
+
+export default function ProfileTopContainer({navigation, route}) {
+  
+  // const {fullName, email, id} = route.params;
+  const [profileData, setProfileData] = useState(fullName="Samira Bahram", email="samirabahrampour@gmail.com")
+
+  return (
+    <StyledContainer> 
+
+      <StatusBar style="dark"/>
+
+      <InnerContainer>
+
+        <View 
+          style={{
+            flex: 1, 
+            justifyContent: 'space-between', 
+            paddingTop: 40, 
+            flexDirection: 'row', 
+            alignItems: 'baseline', 
+            flexWrap: 'wrap',
+            paddingRight: 15
+      }}>
+
+          <Image source={require('../../assets/images/foxicon.png')} 
+            style={{
+              width: 70, 
+              height: 70,
+              borderRadius: 50, 
+              borderWidth: 5, 
+              borderColor: Colors.purple,
+            }}/>
+        
+
+          <View style={{flexDirection: 'row'}}>
+            
+            <Text 
+              style={{
+                fontWeight: 'bold',
+                paddingLeft: 20,
+                paddingHorizontal: 10,
+                paddingRight: 20
+              }}>
+              
+              <Text style={{fontSize:15}}>{fullName}{"\n"}</Text>
+              <Text style={{fontSize:12}}>{email}{"\n"}</Text>
+
+            </Text>
+
+            <TouchableOpacity 
+              activeOpacity={0.5}
+              onPress = {() => navigation.navigate('ToBeDefinedButWorks')}
+            >
+              
+              <Image 
+                source={require('../../assets/images/penPurple.png')} 
+                style={{
+                  width: 17, 
+                  height: 17,
+                }}
+              />
+              
+            </TouchableOpacity>
+
+          </View>
+
+        </View>
+
+      </InnerContainer>
+
+    </StyledContainer>
+  )
+}
+
