ba0e860d70357957e83069a229b04a66797fcc44
icodebyamanda <rasoamanda@gmail.com>
2021-11-25 14:17:25 +0100

    picture & text & pen all aligned

--- screens/ProfileSettings/ProfileSettingsPage.js
+++ screens/ProfileSettings/ProfileSettingsPage.js
@@ -22,36 +22,54 @@ export default function ProfileSettingsPage({navigation, route}) {
 
       <InnerContainer>
 
-        <View style={{flex: 1, justifyContent: 'space-between', padding: 8, paddingTop: 40, flexDirection:'row', alignItems: 'baseline'
+        <View style={{
+          flex: 1, 
+          justifyContent: 'space-between', 
+          paddingTop: 40, 
+          flexDirection: 'row', 
+          alignItems: 'baseline', 
+          flexWrap: "wrap",
+          paddingRight: 15
       }}>
 
         <Image source={require('../../assets/images/foxicon.png')} 
         style={{
           width: 70, 
-          height: 70, 
-          borderRadius: 50, borderWidth: 5, 
-          borderColor: Colors.purple}}/>
+          height: 70,
+          // alignSelf: "center",
+          borderRadius: 50, 
+          borderWidth: 5, 
+          borderColor: Colors.purple,
+          }}/>
 
-        <View 
+        {/* <View 
         style={{
           paddingLeft: 15, 
-          flexDirection:'row'}}>
+          flexDirection:'row'}}> */}
             
           <Text 
           style={{
-            fontSize: 15, 
-            fontWeight: 'bold'}}>
+            fontWeight: 'bold',
+            // alignSelf: "center",
+            paddingLeft: 20,
+            paddingHorizontal: 10,
+            paddingRight: 20
+            }}>
             
-            {fullName}{"\n"}
-            {email}{"\n"}
+            <Text style={{fontSize:15}}>{fullName}{"\n"}</Text>
+            <Text style={{fontSize:12}}>{email}{"\n"}</Text>
 
           </Text>
 
           <Image source={require('../../assets/images/penPurple.png')} 
           style={{
-            width: 15, 
-            height: 15}}/>
-        </View>
+            width: 17, 
+            height: 17,
+            alignSelf: "center",
+            alignItems: "center",
+            flexDirection: 'column'
+            }}/>
+        {/* </View> */}
 
         
 
