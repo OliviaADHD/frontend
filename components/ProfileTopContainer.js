c52ac0d8299dd8ac9843d14cb03655bde69f9c37
icodebyamanda <rasoamanda@gmail.com>
2021-11-25 18:58:09 +0100

    working out the first menu page

--- components/ProfileTopContainer.js
+++ components/ProfileTopContainer.js
@@ -13,61 +13,60 @@ export default function ProfileTopContainer({navigation, route}) {
 
   return (
 
-
-        <View 
-          style={{
-            flex: 1, 
-            justifyContent: 'space-between', 
-            paddingTop: 40, 
-            flexDirection: 'row', 
-            alignItems: 'baseline', 
-            flexWrap: 'wrap',
-            paddingRight: 15
+    <View 
+      style={{
+        flex: 1, 
+        justifyContent: 'space-between', 
+        paddingTop: 40, 
+        flexDirection: 'row', 
+        alignItems: 'baseline', 
+        flexWrap: 'wrap',
+        paddingRight: 15
       }}>
 
-          <Image source={require('../assets/images/foxicon.png')} 
-            style={{
-              width: 70, 
-              height: 70,
-              borderRadius: 50, 
-              borderWidth: 5, 
-              borderColor: Colors.purple,
-            }}/>
+      <Image source={require('../assets/images/foxicon.png')} 
+        style={{
+          width: 70, 
+          height: 70,
+          borderRadius: 50, 
+          borderWidth: 5, 
+          borderColor: Colors.purple,
+        }}/>
         
 
-          <View style={{flexDirection: 'row'}}>
+      <View style={{flexDirection: 'row'}}>
             
-            <Text 
+        <Text 
+          style={{
+            fontWeight: 'bold',
+            paddingLeft: 20,
+            paddingHorizontal: 10,
+            paddingRight: 20
+          }}>
+              
+          <Text style={{fontSize:15}}>{fullName}{"\n"}</Text>
+          <Text style={{fontSize:12}}>{email}{"\n"}</Text>
+
+        </Text>
+
+          <TouchableOpacity 
+            activeOpacity={0.5}
+            onPress = {() => navigation.navigate('ToBeDefined')}
+          >
+              
+            <Image 
+              source={require('../assets/images/penPurple.png')} 
               style={{
-                fontWeight: 'bold',
-                paddingLeft: 20,
-                paddingHorizontal: 10,
-                paddingRight: 20
-              }}>
-              
-              <Text style={{fontSize:15}}>{fullName}{"\n"}</Text>
-              <Text style={{fontSize:12}}>{email}{"\n"}</Text>
-
-            </Text>
-
-            <TouchableOpacity 
-              activeOpacity={0.5}
-              onPress = {() => navigation.navigate('ToBeDefined')}
-            >
-              
-              <Image 
-                source={require('../assets/images/penPurple.png')} 
-                style={{
-                  width: 17, 
-                  height: 17,
+                width: 17, 
+                height: 17,
                 }}
               />
               
-            </TouchableOpacity>
+          </TouchableOpacity>
 
-          </View>
+      </View>
 
-        </View>
+    </View>
 
   )
 }
--- screens/ProfileSettings/ProfileSettingsPage.js
+++ screens/ProfileSettings/ProfileSettingsPage.js
@@ -10,6 +10,8 @@ import {
 
 import ProfileTopContainer from "../../components/ProfileTopContainer";
 
+import Profile from "../Dashboard/Profile";
+
 export default function ProfileSettingsPage({navigation}) {
 
 
@@ -21,6 +23,110 @@ export default function ProfileSettingsPage({navigation}) {
       <InnerContainer>
 
         <ProfileTopContainer/>
+
+        <View
+          style={{
+            // alignContent: "flex-start"
+            // display: "flex",
+            // alignItems: "flex-start",
+            // flexDirection: "column",
+            // justifyContent: "center",
+            // alignItems: "center",
+            // paddingVertical: 200,
+            flex: 1,
+            // paddingVertical: 30,
+            paddingBottom: 400,
+            paddingEnd: 150,
+            justifyContent: "space-evenly",
+
+          }}
+        >
+        
+        {/* <View
+        style={{
+          flex: 1,
+          paddingBottom: 70
+        }}
+        > */}
+        <TouchableOpacity
+          style={{
+            flex: 1,
+            paddingBottom: 50
+          }}
+            activeOpacity={0.5}
+            onPress = {() => navigation.navigate('ToBeDefined')}
+          >
+        <Text>
+          Preferences
+        </Text>
+        </TouchableOpacity>
+
+        {/* </View> */}
+
+        {/* <View
+        style={{
+          paddingBottom: 30
+        }}
+        > */}
+        <TouchableOpacity 
+        style={{
+          flex: 1,
+          paddingBottom: 50
+        }}
+            activeOpacity={0.5}
+            onPress = {() => navigation.navigate('ToBeDefined')}
+          >
+        <Text>
+          Setting and Privacy
+        </Text>
+        </TouchableOpacity>
+        {/* </View> */}
+
+        {/* <View
+        style={{
+          paddingVertical: 20
+        }}
+        > */}
+        <TouchableOpacity 
+        style={{
+          flex: 1,
+          paddingBottom: 50
+        }}
+            activeOpacity={0.5}
+            onPress = {() => navigation.navigate('ToBeDefined')}
+          >
+        <Text>
+          Help
+        </Text>
+        </TouchableOpacity>
+        {/* </View> */}
+
+        {/* <View
+        style={{
+          paddingVertical: 20
+        }}
+        > */}
+        <TouchableOpacity 
+        // style={{
+        //   flex: 1,
+        //   paddingBottom: 50
+        // }}
+            activeOpacity={0.5}
+            onPress = {() => navigation.navigate('Login')}
+          >
+        <Text>
+          Sign Out
+        </Text>
+      </TouchableOpacity>
+      {/* </View> */}
+
+        </View>
+
+        {/* <Profile 
+        style={{
+          paddingBottom: 100
+        }}
+        /> */}
 
       </InnerContainer>
 
