2d316c66bc793bc93856efea76f70f1675d7703c
icodebyamanda <rasoamanda@gmail.com>
2021-11-25 15:53:19 +0100

    code cleaned up about to turn it into a component

--- screens/ProfileSettings/ProfileSettingsPage.js
+++ screens/ProfileSettings/ProfileSettingsPage.js
@@ -6,10 +6,6 @@ import {
   StyledContainer,
   InnerContainer,
   Colors,
-  MediumExtraText,
-  StyledFormArea,
-  StyledButton,
-  ButtonText,
 }from '../../components/styles';
 
 export default function ProfileSettingsPage({navigation, route}) {
@@ -31,55 +27,51 @@ export default function ProfileSettingsPage({navigation, route}) {
             paddingTop: 40, 
             flexDirection: 'row', 
             alignItems: 'baseline', 
-            flexWrap: "wrap",
+            flexWrap: 'wrap',
             paddingRight: 15
       }}>
 
-        <Image source={require('../../assets/images/foxicon.png')} 
-          style={{
-            width: 70, 
-            height: 70,
-            // alignSelf: "center",
-            borderRadius: 50, 
-            borderWidth: 5, 
-            borderColor: Colors.purple,
-          }}/>
+          <Image source={require('../../assets/images/foxicon.png')} 
+            style={{
+              width: 70, 
+              height: 70,
+              borderRadius: 50, 
+              borderWidth: 5, 
+              borderColor: Colors.purple,
+            }}/>
         
 
-        <View style={{flexDirection: "row"}}>
-          <Text 
-            style={{
-              fontWeight: 'bold',
-              // alignSelf: "center",
-              // alignItems: "center",
-              paddingLeft: 20,
-              paddingHorizontal: 10,
-              paddingRight: 20
-            }}>
+          <View style={{flexDirection: 'row'}}>
             
-            <Text style={{fontSize:15}}>{fullName}{"\n"}</Text>
-            <Text style={{fontSize:12}}>{email}{"\n"}</Text>
-
-          </Text>
-
-          <TouchableOpacity 
-            activeOpacity={0.5}
-            onPress = {() => navigation.navigate("ToBeDefinedButWorks")}>
-            
-            <Image 
-              source={require('../../assets/images/penPurple.png')} 
+            <Text 
               style={{
-                width: 17, 
-                height: 17,
-                // alignSelf: "center",
-                // alignItems: "center",
-                // flexDirection: 'row'
-              }}
-            />
-            
-          </TouchableOpacity>
+                fontWeight: 'bold',
+                paddingLeft: 20,
+                paddingHorizontal: 10,
+                paddingRight: 20
+              }}>
+              
+              <Text style={{fontSize:15}}>{fullName}{"\n"}</Text>
+              <Text style={{fontSize:12}}>{email}{"\n"}</Text>
 
-        </View>
+            </Text>
+
+            <TouchableOpacity 
+              activeOpacity={0.5}
+              onPress = {() => navigation.navigate('ToBeDefinedButWorks')}
+            >
+              
+              <Image 
+                source={require('../../assets/images/penPurple.png')} 
+                style={{
+                  width: 17, 
+                  height: 17,
+                }}
+              />
+              
+            </TouchableOpacity>
+
+          </View>
 
         </View>
 
