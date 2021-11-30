8c8d5ef76b277b0890389c3282e0cf3a1e4a3681
icodebyamanda <rasoamanda@gmail.com>
2021-11-30 14:04:09 +0100

    about to modify form layout

--- screens/ProfileSettings/ProfilePreferencesPage.js
+++ screens/ProfileSettings/ProfilePreferencesPage.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, Button, TouchableOpacity } from "react-native";
 import { StatusBar } from "expo-status-bar";
+import { Formik, Field, Form } from 'formik';
 
 import {
   StyledContainer,
   InnerContainer,
   Colors,
-  ProfileListText,
-  ProfileListTouch,
+  // ProfileListText,
+  // ProfileListTouch,
   StyledFormArea
 } from '../../components/styles';
 
@@ -16,6 +17,11 @@ import {
 // import Profile from "../Dashboard/Profile";
 
 export default function ProfilePreferencesPage({navigation}) {
+
+  // const [preferences, setPreferences] = useState({
+  //   language: '',
+  //   mode: false
+  // })
 
   return (
     <StyledContainer> 
@@ -29,48 +35,50 @@ export default function ProfilePreferencesPage({navigation}) {
           <View
             style={{
               // borderTopWidth: 1,
-              borderBottomWidth: 1,
+              borderBottomWidth: 2,
               // borderTopColor: Colors.lightpurple,
               borderBottomColor: Colors.lightpurple, 
-              width: 120
+              width: 120,
+              alignItems: 'center'
             }}
           >
                       
-            <Text
-            style={{
-              paddingLeft: 20
-            }}
-            > Preferences </Text>          
+            <Text> Preferences </Text>          
 
           </View>
 
-          <View
-            style={{
-              borderBottomWidth: 1,
-              borderBottomColor: Colors.lightpurple
-            }}
-          >
-            <TouchableOpacity 
-              activeOpacity={0.5}
-              onPress = {() => navigation.navigate('Login')}
-            >
-              <Text> Language </Text>            
-            </TouchableOpacity>
+        <Formik
+          initialValues={{ language: '', mode: false}}
+          onSubmit={values => console.log(values)}
+        >
+
+{({ handleChange, handleBlur, handleSubmit, values }) => (
+
+          <View>
+
+              <Text
+              style={{
+                borderBottomWidth: 1,
+                borderBottomColor: Colors.lightpurple
+              }}> 
+              Language 
+              </Text>            
+
+              <Text
+              style={{
+                borderBottomWidth: 1,
+                borderBottomColor: Colors.lightpurple
+              }}> 
+              Dark mode 
+              </Text>            
+            
+            <Button onPress={handleSubmit} title='Save all changes' />
+
           </View>
 
-          <View
-            style={{
-              borderBottomWidth: 1,
-              borderBottomColor: Colors.lightpurple
-            }}
-          >
-            <TouchableOpacity
-              activeOpacity={0.5}
-              onPress = {() => navigation.navigate('Login')}
-            >
-              <Text> Dark mode </Text>            
-            </TouchableOpacity>
-          </View>
+)}
+
+      </Formik>
 
         </StyledFormArea>
         {/* <Profile/> */}
