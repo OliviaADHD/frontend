6d0b5440f91ab6f4982dd26d1b0192dc337fefca
AnjaTRPES <roeder.anja.sci@gmail.com>
2021-12-01 08:54:03 +0100

    updated reducer to work with the backend

--- src/actions/menstruation/menstruation.js
+++ src/actions/menstruation/menstruation.js
@@ -2,13 +2,40 @@ import {
     SET_MENS_REGULAR,
     SET_START_LAST_PERIOD, 
     SET_PERIOD_CYCLE_LENGTH,
-    SET_PERIOD_LENGTH
+    SET_PERIOD_LENGTH,
+    SET_FIRST_TIME
 } from "../actions/types";
 
 import axios from "axios";
 import {link} from '../../config/config'
 
 //Menstruation regular not done yet, as I have to change the backend for this!
+
+export const initializeMenstruationAfterLogin = (userId) => async dispatch =>{
+    axios.get(link+"/menstruation"+"/checkUserMenstruationDataExists", userId)
+    .then(resp => axios.get(link+"/menstruation"+"/getUserData", userId))
+    .then(res => res.json())
+    .then(data =>{
+        dispatch({type: SET_MENS_REGULAR, payload: data.regular});
+        dispatch({type: SET_START_LAST_PERIOD, payload: data.LastPeriodStarts});
+        dispatch({type: SET_PERIOD_CYCLE_LENGTH, payload: data.periodCycleLengths});
+        dispatch({type: SET_PERIOD_LENGTH, payload: data.PeriodLengths});
+        dispatch({type: SET_FIRST_TIME, payload: data.firstTime})
+    })
+    .catch(err => {
+        if (!err.response.status) {
+            //it's a network error!
+            dispatch({
+            type: SET_NETWORK_ERROR_TRUE,
+            payload: {}});
+            dispatch({type: SET_FIRST_TIME, payload: "Error"});
+        } else {
+            //Data does not exist yet! It's the first time
+            dispatch({type: SET_FIRST_TIME, payload: true});
+        }
+    });
+
+};
 
 export const newStartPeriod = (login, startPeriod) => async dispatch => {
     try {
--- src/reducers/menstruationReducer.js
+++ src/reducers/menstruationReducer.js
@@ -2,38 +2,45 @@ import {
     SET_MENS_REGULAR,
     SET_START_LAST_PERIOD, 
     SET_PERIOD_CYCLE_LENGTH,
-    SET_PERIOD_LENGTH
+    SET_PERIOD_LENGTH,
+    SET_FIRST_TIME
 } from "../actions/types";
 
 
 const menstruationData = {
+    firstTime: undefined,
     regular: undefined,
-    startLastPeriod: undefined,
-    periodCycleLength: undefined,
-    periodLength: undefined    
+    startLastPeriod: [],
+    periodCycleLength: [],
+    periodLength: []  
 }
 
 const menstruationReducer = (state = menstruationData, action) => {
     switch (action.type){
+        case SET_FIRST_TIME:
+            return{
+                ...state,
+                firstTime: action.payload
+            };
         case SET_MENS_REGULAR:
             return{
                 ...state,
-                regular: action.payload.regular
+                regular: action.payload
             };
         case SET_START_LAST_PERIOD:
             return{
                 ...state,
-                startLastPeriod: action.payload.startLastPeriod
+                startLastPeriod: action.payload
             };
         case SET_PERIOD_CYCLE_LENGTH:
             return{
                 ...state,
-                periodCycleLength: action.payload.periodCycleLength
+                periodCycleLength: action.payload
             };
         case SET_PERIOD_LENGTH:
             return{
                 ...state,
-                periodLength: action.payload.periodLength
+                periodLength: action.payload
             };
         default:
             return state;
