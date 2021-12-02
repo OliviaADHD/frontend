8132c22cfc13699222b630c32d7c22c5e27d3ed1
AnjaTRPES <roeder.anja.sci@gmail.com>
2021-12-02 12:41:14 +0100

    monthlyCalender looks good!

--- screens/Menstruation/MonthlyCalendar.js
+++ screens/Menstruation/MonthlyCalendar.js
@@ -33,15 +33,16 @@ const MonthlyCalendar =({pageNav, setPage}) => {
                     <ButtonGroup style={{width: "62%", height: "100%",
                                         left: "0%", top: "0%", padding: "1%", alignItems: "center"}}>
                         <ButtonGroupChild style={{backgroundColor: Colors.purple, alignItems: "center"}}>
-                            <ButtonText  style={{color: 'white'}}>Month</ButtonText>
+                            <ButtonText  style={{color: Colors.white}}>Month</ButtonText>
                         </ButtonGroupChild>
                         <ButtonGroupChild>
                             <ButtonText onPress = {() => setPage(pageNav.YearlyCalendar)} 
                             style={{color: 'black'}}>Year</ButtonText>
                         </ButtonGroupChild>
                     </ButtonGroup>
+                </ButtonGroupContainer>
                 
-                    <StyledCalendar style={{marginTop: "4%", marginBottom: "2%", height: "50%"}}>
+                <StyledCalendar style={{marginTop: "4%", marginBottom: "2%", height: "50%"}}>
                         <Calendar
                             current={Date()}
                             onDayPress={(day) =>console.log(day)}
@@ -69,13 +70,13 @@ const MonthlyCalendar =({pageNav, setPage}) => {
                         
                         />
                     </StyledCalendar>
-                </ButtonGroupContainer>
+                
                 <ButtonContainers style={{width: "70%",
-                                          marginTop: "0%", top: "85%", alignItems: "center"}}>
+                                          marginTop: "0%", top: "5%", alignItems: "center"}}>
                     <InputMoodBtn><ButtonText onPress = {() => setPage(pageNav.Mood)} >Input Mood</ButtonText></InputMoodBtn>
                     <EditPeriodBtn><ButtonText onPress = {() => setPage(pageNav.Period)} >Input Period</ButtonText></EditPeriodBtn>
                 </ButtonContainers>
-                <MediumExtraText style={{ color : Colors.black, margin : '0%', width: "60%", top: "63%",
+                <MediumExtraText style={{ color : Colors.black, margin : '0%', width: "60%", top: "6%",
                                             textDecorationLine: "underline",
                                             padding: "0%", paddingTop: "0%", paddingBottom: "0%",
                                         height: "7%"}}>Mood Analysis Chart</MediumExtraText>
--- screens/Menstruation/YearlyCalendar.js
+++ screens/Menstruation/YearlyCalendar.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import {
-    StyledContainer,
+    InnerContainer,
+    Colors,
     ButtonText,
     ButtonGroup,
     ButtonGroupChild,
@@ -15,12 +16,20 @@ const YearlyCalendar =({pageNav, setPage}) => {
 
 
     return (
-        <StyledContainer>  
-            <StatusBar style="dark"/>
-                <ButtonGroupContainer>
-                    <ButtonGroup>
-                        <ButtonGroupChild><ButtonText onPress = {() => setPage(pageNav.MonthlyCalendar)} style={{color: 'black'}}>Month</ButtonText></ButtonGroupChild>
-                        <ButtonGroupChild><ButtonText  style={{color: 'black'}}>Year</ButtonText></ButtonGroupChild>
+        <InnerContainer>  
+                <ButtonGroupContainer style={{marginTop: "8%", 
+                                        height: "6%",
+                                        alignItems: "center"}}>
+                    <ButtonGroup style={{width: "62%", height: "100%",
+                                        left: "0%", top: "0%", padding: "1%", alignItems: "center"}}>
+                        <ButtonGroupChild style={{backgroundColor: Colors.white, alignItems: "center"}}>
+                            <ButtonText  style={{color: Colors.black}}
+                                onPress = {() => setPage(pageNav.MonthlyCalendar)}
+                            >Month</ButtonText>
+                        </ButtonGroupChild>
+                        <ButtonGroupChild style={{backgroundColor: Colors.purple}}>
+                            <ButtonText style={{color: Colors.white}}>Year</ButtonText>
+                        </ButtonGroupChild>
                     </ButtonGroup>
                 </ButtonGroupContainer>
                 <StyledCalendarList>
@@ -29,7 +38,7 @@ const YearlyCalendar =({pageNav, setPage}) => {
                             onDayPress={(day) =>onDayPress(day)}
                         />
                 </StyledCalendarList>
-        </StyledContainer> 
+        </InnerContainer> 
     );
 }
 
