b30846f5dbe59cf0d3bc9a7128215f1b50bcf569
device999 <sattari4@hotmail.com>
2021-12-03 01:26:55 +0100

    refactored alarmbell css

--- src/components/AlarmBell.js
+++ src/components/AlarmBell.js
@@ -3,41 +3,13 @@ import {Text, ImageBackground, StyleSheet, View} from 'react-native';
 
 
 import { Colors } from "../css/general/style";
-
-
-const stylesBell = StyleSheet.create({
-    externalContainer: {
-        justifyContent: "flex-end",
-        alignItems: "flex-end",
-        height: "100%",
-        width: "100%",
-    },
-    containerView: {
-        flexDirection: "column",
-        justifyContent: "flex-end",
-        height: "100%",
-        width: "10%",
-        marginRight: "5%"
-    },
-    bellImage: {
-        height: "80%",
-        width: "100%",
-    },
-    roundView: {
-        backgroundColor: "red",
-        height: "40%",
-        width: "40%",
-        borderRadius: 9,
-        marginLeft: "10%",
-        marginTop: "-10%",
-        justifyContent: "center"
-    },
-    roundNumber: {
-        fontSize: 10,
-        color: Colors.white,
-        textAlign: "center",
-    }
-});
+import {
+    ExternalContainer,
+    ContainerView,
+    BellImage,
+    RoundNumber,
+    RoundView
+} from "../css/components/alarm"
 
 class AlarmBell extends React.Component{
     constructor(props) {
@@ -46,18 +18,17 @@ class AlarmBell extends React.Component{
     }
     render(){
         return (
-            <View style={stylesBell.externalContainer}>
-                <View style={stylesBell.containerView}>
-                    <ImageBackground source={require('../../assets/images/Bell.png')} 
-                                    resizeMode="contain" style={stylesBell.bellImage}>
+            <ExternalContainer>
+                <ContainerView>
+                    <BellImage source={require('../../assets/images/Bell.png')} resizeMode="contain">
                     {this.props.showAlarm &&
-                        <View style={stylesBell.roundView}>
-                            <Text style={stylesBell.roundNumber}>{this.props.alarmNumber}</Text>
-                        </View>
+                        <RoundView>
+                            <RoundNumber>{this.props.alarmNumber}</RoundNumber>
+                        </RoundView>
                     }
-                    </ImageBackground>
-                </View>
-            </View>
+                    </BellImage>
+                </ContainerView>
+            </ExternalContainer>
         );}
 }
 
