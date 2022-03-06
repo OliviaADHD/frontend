3fffa9272f8718d17e0accd27fa036a0b06296b9
AnjaTRPES <roeder.anja.sci@gmail.com>
2022-03-06 18:51:42 +0100

    adds Calendar picker

--- src/css/Dashboard/New/createEvent.js
+++ src/css/Dashboard/New/createEvent.js
@@ -45,7 +45,6 @@ export const EventView = styled.View`
   alignItems: center;
 `;
 
-
 export const InputView = styled.TextInput`
     height:100%;
     marginLeft:5%;
@@ -59,4 +58,37 @@ export const InputViewTitle = styled.TextInput`
     fontSize: 20px;
     borderBottomColor: #FFFFFF;
     width:65%;
+`;
+
+export const CalendarDateTouchable = styled.TouchableOpacity`
+    height: 100%;
+    marginLeft: 5%;
+    width: 20%;
+    flexDirection: row;
+    justifyContent: space-between;
+    alignItems: center;
+    alignContent: center;
+`;
+
+export const DarkGrayText = styled.Text`
+    color: ${Colors.darkgray};
+    backgroundColor: blue;
+    
+`;
+
+export const PurpleButton = styled.TouchableOpacity`
+    backgroundColor: ${Colors.purple};
+    height: 15%;
+    marginTop: 5%;
+    borderRadius: 30px;
+    width: 80%;
+    alignSelf: center;
+    justifyContent: center;
+`;
+
+export const WhiteText = styled.Text`
+    alignSelf: center;
+    color: ${Colors.white};
+    fontWeight: bold;
+    fontSize: 18px;
 `;
--- src/screens/Dashboard/New/Event.js
+++ src/screens/Dashboard/New/Event.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
 import {StatusBar} from "expo-status-bar";
-import {TouchableOpacity, Dimensions, Text} from 'react-native';
+import {TouchableOpacity, View, Dimensions, Text} from 'react-native';
 import {Colors, StyledContainer} from "../../../css/general/style";
 import { Icon } from 'react-native-elements';
 import { HeaderView, 
@@ -10,9 +10,15 @@ import { HeaderView,
     CloseView, 
     StyledIcon, 
     InputView, 
-    InputViewTitle } from "../../../css/Dashboard/New/createEvent";
+    InputViewTitle,
+    CalendarDateTouchable,
+    DarkGrayText,
+    PurpleButton,
+    WhiteText } from "../../../css/Dashboard/New/createEvent";
 
 import { useSelector, useDispatch } from "react-redux";
+
+import {Calendar} from 'react-native-calendars';
 
 const windowWidth = Dimensions.get('window').width;
 
@@ -26,10 +32,22 @@ export const Event = ({navigation}) => {
         navigation.navigate("ToDoList");
     };
 
+    var today = new Date(); 
+
+    const transformDateToFormatForCalendar = (date) => {
+        let dateString = date.getFullYear()+"-"
+        const month = (date.getMonth() > 8)? (date.getMonth()+1) : "0" + (date.getMonth()+1);
+        const day = (date.getDate() > 9)? date.getDate() : "0" + date.getDate();
+        return dateString+month+'-'+day;
+    }
+
     const [inputText, setInputText] = useState("");
 
     const [titleText, setTitleText] = useState("");
     const [detailsText, setDetailsText] = useState("");
+    const [showCalendar, setShowCalendar] = useState(false);
+    const [selectedDate, setSelectedDate] = useState(transformDateToFormatForCalendar(today));
+
 
     return (
         <StyledContainer>
@@ -53,92 +71,120 @@ export const Event = ({navigation}) => {
                                 value={titleText}
                                 onChangeText={(text) => setTitleText(text)}/>
                     </EventView>
-                    <EventView>
-                        <Icon
-                        name='menu-outline'
-                        type='ionicon'
-                        size={35}
-                        style={{
-                            marginLeft:"10%"
-                        }}
-                        />
-                        <InputView
-                            placeholder="Add Details"
-                            underlineColorAndroid='transparent'
-                            multiline={true}
-                            value={detailsText}
-                            onChangeText={(text) => setDetailsText(text)}/>
-                    </EventView>
-                    <EventView>
-                        <Icon
-                        name='calendar-outline'
-                        type='ionicon'
-                        size={35}
-                        style={{
-                            marginLeft:"10%"
-                        }}
-                        />
-                        <InputView
-                            placeholder="Date different input?"
-                            underlineColorAndroid='transparent'
-                            value={inputText}
-                            onChangeText={(text) => setInputText(text)}/>
-                    </EventView>
-                    <EventView>
-                        <Icon
-                        name='time-outline'
-                        type='ionicon'
-                        size={35}
-                        style={{
-                            marginLeft:"10%"
-                        }}
-                        />
-                        <InputView
-                            placeholder="From"
-                            underlineColorAndroid='transparent'
-                            value={inputText}
-                            onChangeText={(text) => setInputText(text)}/>
-                    </EventView>
-                    <EventView>
-                        <InputView
-                            placeholder="To"
-                            underlineColorAndroid='transparent'
-                            value={inputText}
-                            onChangeText={(text) => setInputText(text)}/>
-                    </EventView>
-                    <EventView>
-                        <Icon
-                        name='alarm-outline'
-                        type='ionicon'
-                        size={35}
-                        style={{
-                            marginLeft:"10%"
-                        }}
-                        />
-                        <InputView
-                            placeholder="Remind Me"
-                            underlineColorAndroid='transparent'
-                            value={inputText}
-                            onChangeText={(text) => setInputText(text)}/>
-                    </EventView>
-                    <EventView>
-                        <Icon
-                        name='location-outline'
-                        type='ionicon'
-                        size={35}
-                        style={{
-                            marginLeft:"10%"
-                        }}
-                        />
-                        <InputView
-                            placeholder="Location"
-                            underlineColorAndroid='transparent'
-                            value={inputText}
-                            onChangeText={(text) => setInputText(text)}/>
-                    </EventView>
-                    <EventView style={{backgroundColor: Colors.purple}}>
-                        <Text>Done</Text>
-                    </EventView>
+                    {showCalendar?
+                        (<View style={{height: "80%"}}>
+                            <View>
+                                <Calendar
+                                    onDayPress={(day) => setSelectedDate(day.dateString)}
+                                    markedDates={{[selectedDate]: {selected:true, selectedColor: Colors.purple}}}
+                                    theme={{
+                                            
+                                            todayTextColor: '#7047EB',
+                                            dayTextColor: '#333333',
+                                            textDisabledColor: '#BDBDBD',
+                                            arrowColor: '#7047EB',
+                                            monthTextColor: '#333333',
+                                            textDayFontWeight: '500',
+                                            textMonthFontWeight: 'bold',
+                                            textDayHeaderFontWeight: 'normal',
+                                            textDayFontSize: 15,
+                                            textMonthFontSize: 18,
+                                            textDayHeaderFontSize: 15,
+                                            textSectionTitleColor: '#7047EB'
+                                        }}
+                                />
+                            </View>
+                            <PurpleButton onPress={()=>setShowCalendar(false)}>
+                                <WhiteText>Done</WhiteText>
+                            </PurpleButton>
+                        </View>) : (
+                    <View style={{height: "100%"}}>
+                        <EventView>
+                            <Icon
+                            name='menu-outline'
+                            type='ionicon'
+                            size={35}
+                            style={{
+                                marginLeft:"10%"
+                            }}
+                            />
+                            <InputView
+                                placeholder="Add Details"
+                                underlineColorAndroid='transparent'
+                                multiline={true}
+                                value={detailsText}
+                                onChangeText={(text) => setDetailsText(text)}/>
+                        </EventView>
+                        <EventView>
+                            <Icon
+                            name='calendar-outline'
+                            type='ionicon'
+                            size={35}
+                            style={{
+                                marginLeft:"10%"
+                            }}
+                            />
+                            <CalendarDateTouchable onPress={() => setShowCalendar(true)}>
+                                <DarkGrayText>{selectedDate}</DarkGrayText>
+                            </CalendarDateTouchable>
+                        </EventView>
+                        <EventView>
+                            <Icon
+                            name='time-outline'
+                            type='ionicon'
+                            size={35}
+                            style={{
+                                marginLeft:"10%"
+                            }}
+                            />
+                            <CalendarDateTouchable style={{width: "50%"}}>
+                                <DarkGrayText>From</DarkGrayText>
+                                <Text style={{backgroundColor: "red"}}>some Time</Text>
+                            </CalendarDateTouchable>
+
+                        </EventView>
+                        <EventView>
+                            <InputView
+                                placeholder="To"
+                                underlineColorAndroid='transparent'
+                                value={inputText}
+                                onChangeText={(text) => setInputText(text)}/>
+                        </EventView>
+                        <EventView>
+                            <Icon
+                            name='alarm-outline'
+                            type='ionicon'
+                            size={35}
+                            style={{
+                                marginLeft:"10%"
+                            }}
+                            />
+                            <InputView
+                                placeholder="Remind Me"
+                                underlineColorAndroid='transparent'
+                                value={inputText}
+                                onChangeText={(text) => setInputText(text)}/>
+                        </EventView>
+                        <EventView>
+                            <Icon
+                            name='location-outline'
+                            type='ionicon'
+                            size={35}
+                            style={{
+                                marginLeft:"10%"
+                            }}
+                            />
+                            <InputView
+                                placeholder="Location"
+                                underlineColorAndroid='transparent'
+                                value={inputText}
+                                onChangeText={(text) => setInputText(text)}/>
+                        </EventView>
+                        <EventView style={{backgroundColor: Colors.purple}}>
+                            <Text>Done</Text>
+                        </EventView>
+                    </View>)
+                    }
                 </ContentView>
             </InnerContainerRemake>
         </StyledContainer>
